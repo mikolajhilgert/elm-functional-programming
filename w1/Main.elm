e7b2a66576590eba69d68c57334b6f771ce0c96f
Mikolaj Hilgert <456096@student.fontys.nl>
2022-02-08 14:31:53 +0100

    added decode for Caesar p1

--- w1/Main.elm
+++ w1/Main.elm
@@ -10,7 +10,7 @@ import Char exposing (fromCode, toCode, isUpper, isLower)
 
 
 --
--- Strings
+-- Encode & Decode
 --
 
 
@@ -18,25 +18,41 @@ encode: Int -> Char -> Char
 encode offset char =
     if isUpper char then
         if toCode char + offset > toCode 'Z' then
-           fromCode (((toCode char + offset) - toCode 'Z') + (toCode 'A' - 1))
+            fromCode (((toCode char + offset) - toCode 'Z') + (toCode 'A' - 1))
         else fromCode (toCode char + offset)
     else if isLower char then
-     if toCode char + offset > toCode 'z' then
-               fromCode (((toCode char + offset) - toCode 'z') + (toCode 'a' - 1))
-            else fromCode (toCode char + offset)
+        if toCode char + offset > toCode 'z' then
+            fromCode (((toCode char + offset) - toCode 'z') + (toCode 'a' - 1))
+        else fromCode (toCode char + offset)
     else char
 
-
-
-
+decode: Int -> Char -> Char
+decode offset char =
+    if isUpper char then
+        if toCode char - offset < toCode 'A' then
+            fromCode (((toCode char - offset) - toCode 'A') + (toCode 'Z' + 1))
+        else fromCode (toCode char - offset)
+    else if isLower char then
+        if toCode char - offset < toCode 'a' then
+            fromCode (((toCode char - offset) - toCode 'a') + (toCode 'z' + 1))
+        else fromCode (toCode char - offset)
+    else char
 
 examples : List ( String, List ExerciseRunner.Example )
 examples =
     [ ( "Caesar (part 1)"
-      , [ ExerciseRunner.functionExample2 ""
+      , [ ExerciseRunner.functionExample2 "encode"
             encode
             [ ( (5, 'x'), 'c' )
             , ( (7, 'T'), 'A' )
+            ]
+        ]
+      ),
+      ( ""
+      , [ ExerciseRunner.functionExample2 "decode"
+            decode
+            [ ( (5, 'c'), 'x' )
+            , ( (7, 'A'), 'T' )
             ]
         ]
       )
